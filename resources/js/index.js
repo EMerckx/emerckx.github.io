ccf665b8664dcb1b50106b3c0626fa9920752e6c
Ewout Merckx <ewmerckx.merckx@ugent.be>
2015-06-22 23:57:50 +0200

    fullPage.js added

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,1 +1,16 @@
+$(document).ready(function() {
 
+
+    setFullPages();
+});
+
+function setFullPages() {
+    $('#fullpage').fullpage({
+        autoScrolling: true,
+        fitToSection: false,
+        'sectionsColor': ['#3D577A', '#677C97', '#3D577A', '#677C97'],
+        navigation: true,
+        'navigationPosition': 'right',
+        'navigationTooltips': ['Overview', 'Experience', 'Education', 'About']
+    });
+}
